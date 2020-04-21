9c095d15bcf9c2a60bfe922690bf4b22eea9753f
Sandy Satriya <sandy@multiinti.com>
2020-04-22 01:19:22 +0700

    add unittest

---
+++ tests/test_signup.py
@@ -0,0 +1,22 @@
+import unittest
+import json
+
+from tests.BaseCase import BaseCase
+
+
+class SignupTest(BaseCase):
+
+    def test_successful_signup(self):
+        # Given
+        payload = json.dumps({
+            "email": "jamal@mail.com",
+            "password": "mycoolpassword"
+        })
+
+        # When
+        response = self.app.post(
+            '/api/auth/signup', headers={"Content-Type": "application/json"}, data=payload)
+
+        # Then
+        self.assertEqual(str, type(response.json['id']))
+        self.assertEqual(200, response.status_code)
